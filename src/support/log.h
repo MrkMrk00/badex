a0d18cc0f4396fbc30528a260e0b6ca92ad4a5ad
MrkMrk00 <mhau@outlook.cz>
2025-07-31 18:40:47 +0200

    chore(formatting): fix log.h format

--- src/support/log.h
+++ src/support/log.h
@@ -4,34 +4,35 @@
 #include <stdio.h>
 
 // main logging statement
-#define _BX_DO_LOG(file, level, message, ...) fprintf((file), "%s:%d: [%s] " message, __FILE__, __LINE__, (level), __VA_ARGS__)
+#define _BX_DO_LOG(file, level, message, ...)                                                                          \
+    fprintf((file), "%s:%d: [%s] " message, __FILE__, __LINE__, (level), __VA_ARGS__)
 
 // this always logs (errors and stuff)
 #define BX_NOTICE(message, ...) _BX_DO_LOG(stdout, "NOTICE", message, __VA_ARGS__)
 #define BX_ERROR(message, ...) _BX_DO_LOG(stderr, "ERROR", message, __VA_ARGS__)
 
 // Non recoverable errors
-#define BX_FATAL(...)                                                                                                                                          \
-    do {                                                                                                                                                       \
-        BX_ERROR(__VA_ARGS__);                                                                                                                                 \
-        exit(1);                                                                                                                                               \
+#define BX_FATAL(...)                                                                                                  \
+    do {                                                                                                               \
+        BX_ERROR(__VA_ARGS__);                                                                                         \
+        exit(1);                                                                                                       \
     } while (0)
 
-#define BX_PFATAL(message)                                                                                                                                     \
-    do {                                                                                                                                                       \
-        BX_ERROR("%s", "");                                                                                                                                    \
-        perror(message);                                                                                                                                       \
-        exit(1);                                                                                                                                               \
+#define BX_PFATAL(message)                                                                                             \
+    do {                                                                                                               \
+        BX_ERROR("%s", "");                                                                                            \
+        perror(message);                                                                                               \
+        exit(1);                                                                                                       \
     } while (0)
 
 // runtime assert is always available
-#define BX_RTASSERT(cond, message)                                                                                                                             \
-    if (!(cond))                                                                                                                                               \
+#define BX_RTASSERT(cond, message)                                                                                     \
+    if (!(cond))                                                                                                       \
     BX_FATAL("(%s): %s\n", #cond, (message))
 
 // PASSERT is always available - only use for stuff that should crash the program
-#define BX_PASSERT(cond)                                                                                                                                       \
-    if (!(cond))                                                                                                                                               \
+#define BX_PASSERT(cond)                                                                                               \
+    if (!(cond))                                                                                                       \
     BX_PFATAL(#cond)
 
 #ifdef DEBUG
@@ -39,8 +40,8 @@
 #define BX_INFOS(level, message) BX_INFO(level, "%s\n", (message))
 
 // runtime assert only defined when -DDEBUG is provided
-#define BX_ASSERT(cond, message)                                                                                                                               \
-    if (!(cond))                                                                                                                                               \
+#define BX_ASSERT(cond, message)                                                                                       \
+    if (!(cond))                                                                                                       \
     BX_FATAL("(%s): %s\n", #cond, (message))
 #else
 #define BX_INFO(...)
